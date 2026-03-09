7a639ace42a60e3583a4a5a7b5c072163fbae750
jbboncay17 <jbboncay1@gmail.com>
2026-03-09 23:11:47 +0800

    final one

--- check_the_beginning.py
+++ check_the_beginning.py
@@ -2,10 +2,9 @@ bold="\033[1m"
 end="\033[0m"
 red="\033[31m"
 green="\033[32m"
-black="\033[30m"
 
-text=input(bold+black+"Enter your text:"+end)
-word=input(bold+black+"Enter the starting word/letter to check:"+end)
+text=input(bold+"Enter your text:"+end)
+word=input(bold+"Enter the starting word/letter to check:"+end)
 print("")
 if text[:len(word)]==word:
     print(bold+green+"Correct"+end)
