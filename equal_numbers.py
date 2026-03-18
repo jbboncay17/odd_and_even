9dc1323865262b37902d07e3dd3b825e7c1be055
jbboncay17 <jbboncay1@gmail.com>
2026-03-18 20:15:02 +0800

    added equal and not equal

--- equal_numbers.py
+++ equal_numbers.py
@@ -1,2 +1,7 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
+
+if num1 == num2:
+    print("The two numbers are Equal")
+else:
+    print("The two numbers are Not Equal")
