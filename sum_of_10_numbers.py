33cc0ee26f647cf7db8fd34caa393dc2df294543
jbboncay17 <jbboncay1@gmail.com>
2026-03-18 22:20:35 +0800

    final changes for the source code

--- sum_of_10_numbers.py
+++ sum_of_10_numbers.py
@@ -1,5 +1,11 @@
-for i in range(10):
-    number = float(input(f"Enter a number {i+1}: "))
-    sum += number
+bold="\033[1m"
+end="\033[0m"
+green="\033[32m"
 
-print("The sum of all 10 numbers is=",sum )
+sum=0
+
+for i in range(10):
+    number = float(input(bold+f"Enter a number {i+1}: "+end))
+    sum += number
+print("")
+print(bold+green+"The sum of all 10 numbers is="+end,sum )
