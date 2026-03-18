e86c92baae16e5bddc168b9dad58c1ce64f9d565
jbboncay17 <jbboncay1@gmail.com>
2026-03-18 20:07:56 +0800

    added if and elif command

--- bigger_number.py
+++ bigger_number.py
@@ -1,2 +1,7 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
+
+if num1>num2:
+    print("The bigger number is:", num1)
+elif num2>num1:
+    print("The bigger number is:", num2)
