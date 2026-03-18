36475b1011b5e0249dd8902743e2e814dae5513a
jbboncay17 <jbboncay1@gmail.com>
2026-03-18 20:10:35 +0800

    final code modification

--- bigger_number.py
+++ bigger_number.py
@@ -1,7 +1,11 @@
-num1=int(input("Enter the first number:"))
-num2=int(input("Enter the second number:"))
+bold='\033[1m'
+end='\033[0m'
+green='\033[32m'
 
+num1=int(input(bold+"Enter the first number:"+end))
+num2=int(input(bold+"Enter the second number:"+end))
+print("")
 if num1>num2:
-    print("The bigger number is:", num1)
+    print(bold+green+"The bigger number is:"+end, num1)
 elif num2>num1:
-    print("The bigger number is:", num2)
+    print(bold+green+"The bigger number is:"+end, num2)
